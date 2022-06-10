2942b24ba062f33ba31421e1b6ac44002ede4144
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-06-10 11:38:24 -0300

    Add files via upload

---
+++ uri1012/uri1012/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+namespace uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, B, C, TRIANGULO, CIRCULO, TRAPEZIO, QUADRADO, RETANGULO;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            TRIANGULO = A * C / 2.0;
+            CIRCULO = Math.Pow(C, 2) * 3.14159;
+            TRAPEZIO = (A + B) * C / 2.0;
+            QUADRADO = B * B;
+            RETANGULO = A * B;
+
+            Console.WriteLine("TRIANGULO: " + TRIANGULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + CIRCULO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + TRAPEZIO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + QUADRADO.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + RETANGULO.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
