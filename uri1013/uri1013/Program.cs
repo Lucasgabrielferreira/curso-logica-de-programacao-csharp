b9416d905b32688a96b4f65715014406979d6b9c
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-06-10 11:52:36 -0300

    Add files via upload

---
+++ uri1013/uri1013/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace uri1012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, Z, maiorXY, maior;
+
+            string[] valores = Console.ReadLine().Split(' ');
+            X = int.Parse(valores[0], CultureInfo.InvariantCulture);
+            Y = int.Parse(valores[1], CultureInfo.InvariantCulture);
+            Z = int.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            maiorXY = (X + Y + Math.Abs(X - Y)) / 2;
+
+            maior = (maiorXY + Z + Math.Abs(maiorXY - Z)) / 2;
+
+            Console.WriteLine(maior + " eh o maior");
+        }
+    }
+}
+
