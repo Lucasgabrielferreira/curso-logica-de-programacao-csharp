c04cbafe18758549c2e2c86be21ff3d8e0ea2bb4
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-06-29 15:42:35 -0300

    Add files via upload

---
+++ uri1044/uri1044/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace uri1044
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] valor = Console.ReadLine().Split(' ');
+            int A = int.Parse(valor[0]);
+            int B = int.Parse(valor[1]);
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }          
+        }
+    }
+}
