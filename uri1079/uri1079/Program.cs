ce16478f9f20ff43b7228ab3d30ea6d807661e5c
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-07-27 10:50:47 -0300

    Add files via upload

---
+++ uri1079/uri1079/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace uri1079
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
