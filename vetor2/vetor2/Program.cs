e89a4c4a8b3437981deb21ccf7c7bbbb2fdeab79
LUCAS GABRIEL FERREIRA <105209565+Lucasgabrielferreira@users.noreply.github.com>
2022-08-03 11:32:44 -0300

    Vetores

---
+++ vetor2/vetor2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace vetor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            int[] vet = new int[N];
+
+            String[] valores = Console.ReadLine().Split(' ');
+
+            for(int i = 0; i < N; i++)
+            {
+                vet[i] = int.Parse(valores[i]);
+            }
+
+            for(int i = 0; i < N; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    Console.WriteLine(vet[i] + " ");
+                }
+            }
+            Console.WriteLine();
+
+            int quantidadesDePares = 0;
+            for (int i = 0; i < N; i++)
+            {
+                if (vet[i] % 2 == 0)
+                {
+                    quantidadesDePares++;
+                }
+            }
+            Console.WriteLine(quantidadesDePares);
+        }
+    }
+}
